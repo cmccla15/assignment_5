dce5d8c847093ed6a18194f654416b7f69366846
cmccla15 <carrickmcclain@gmail.com>
2018-03-27 19:19:17 -0400

    added code

--- reduction.cu
+++ reduction.cu
@@ -1,3 +1,18 @@
 //reduction.cu
 //made by Carrick McClain
 
+#define SECTION_SIZE 10
+#define BLOCK_SIZE 16
+
+__global__ void reduction_add ()
+{
+    __shared__ float XY[]
+
+    for (unsigned int stride = 1; stride <= BLOCK_SIZE; stride *= 2)
+    {
+        int index = (threadIdx.x + 1) * stride * 2 - 1;
+        if (index < 2 * BLOCK_SIZE)
+            XY[index] += XY[index - stride];
+        __syncthreads();
+    }
+}
