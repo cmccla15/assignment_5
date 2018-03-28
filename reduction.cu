b6ca4a9201f5f5152f809f624211abecc8ad1f78
cmccla15 <carrickmcclain@gmail.com>
2018-03-28 00:50:43 -0400

    changes

--- reduction.cu
+++ reduction.cu
@@ -1,23 +1,48 @@
-//reduction.cu
-//made by Carrick McClain
+
+/*
+    Reduction summation algorithm (with sequential addressing)
+    made by: Carrick McClain
+
+    Sources:
+        http://developer.download.nvidia.com/compute/cuda/1.1-Beta/x86_website/projects/reduction/doc/reduction.pdf
+*/
 
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <iostream>
 
-#define SECTION_SIZE 10
 #define BLOCK_SIZE 16
 
-__global__ void reduction_add ()
-{
-    __shared__ float XY[]
+using namespace std;
 
-    for (unsigned int stride = 1; stride <= BLOCK_SIZE; stride *= 2)
+inline void gpu_handle_error( cudaError_t err, const char* file, int line, int abort = 1 )
+{
+	if (err != cudaSuccess)
+	{
+		fprintf (stderr, "gpu error %s, %s, %d\n", cudaGetErrorString (err), file, line);
+		if (abort)
+			exit (EXIT_FAILURE);
+	}
+}
+#define gpu_err_chk(e) {gpu_handle_error( e, __FILE__, __LINE__ );}
+
+__global__ void reduction_add (float* X, float* Y)
+{
+    extern __shared__ float XY[];
+
+    unsigned int tx = threadIdx.x;
+    unsigned int i = blockIdx.x * (2 * blockDim.x) + threadIdx.x;
+    XY[tx] = X[i] + X[i + blockDim.x];
+    __syncthreads();
+
+    for (unsigned int stride = blockDim.x/2; stride > 0; stride >>= 2)
     {
-        int index = (threadIdx.x + 1) * stride * 2 - 1;
-        if (index < 2 * BLOCK_SIZE)
-            XY[index] += XY[index - stride];
+        if (tx < stride)
+            XY[tx] += XY[tx + stride];
         __syncthreads();
     }
+    if (ty == 0)
+        Y[blockIdx.x] = XY[0];
 }
 
 int main (int argc, char** argv)
@@ -25,5 +50,10 @@ int main (int argc, char** argv)
     if (argc < 2)
     {
         cerr << "expected args: input file name" << endl;
+        exit (-1);
     }
+
+
+
+    return 0;
 }
